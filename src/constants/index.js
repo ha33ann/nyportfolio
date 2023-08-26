1778adfb26de72de86386d0bbdc0391fe2f7c599
Hassan <91528684+ha33ann@users.noreply.github.com>
2023-08-26 19:09:02 +0200

    WIP

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,7 +16,7 @@ import {
   figma,
   docker,
   meta,
-  starbucks,
+  java,
   tesla,
   shopify,
   carrent,
@@ -116,11 +116,11 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
+    title: "Software Developer",
+    company_name: "Skolon AB",
+    icon: java,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "Jan 2023 - Mar 2023",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -131,7 +131,7 @@ const experiences = [
   {
     title: "React Native Developer",
     company_name: "Tesla",
-    icon: tesla,
+    icon: java,
     iconBg: "#E6DEDD",
     date: "Jan 2021 - Feb 2022",
     points: [
@@ -144,7 +144,7 @@ const experiences = [
   {
     title: "Web Developer",
     company_name: "Shopify",
-    icon: shopify,
+    icon: java,
     iconBg: "#383E56",
     date: "Jan 2022 - Jan 2023",
     points: [
@@ -157,7 +157,7 @@ const experiences = [
   {
     title: "Full stack Developer",
     company_name: "Meta",
-    icon: meta,
+    icon: java,
     iconBg: "#E6DEDD",
     date: "Jan 2023 - Present",
     points: [
