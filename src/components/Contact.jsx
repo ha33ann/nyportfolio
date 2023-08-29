e83dcdc4071053d2c35c993aeafc71ca6f51188d
Hassan <91528684+ha33ann@users.noreply.github.com>
2023-08-29 16:27:09 +0200

    WIP

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,9 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+//q7V7HrmfK44r2fo2F
+//template_j6axtod
+//service_a062p84
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -17,8 +20,48 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    //q7V7HrmfK44r2fo2F
+    //template_j6axtod
+    //service_a062p84
+
+    emailjs
+      .send(
+        "service_nnz191o",
+        "template_l7645sj",
+        {
+          from_name: form.name,
+          to_name: "Hassan",
+          from_email: form.email,
+          to_email: "hassan_siala@hotmail.com",
+          message: form.message,
+        },
+        "q7V7HrmfK44r2fo2F"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible!");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
@@ -39,6 +82,7 @@ const Contact = () => {
               type="text"
               name="name"
               value={form.name}
+              onChange={handleChange}
               placeholder="What's your name?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
@@ -49,6 +93,7 @@ const Contact = () => {
               type="email"
               name="email"
               value={form.email}
+              onChange={handleChange}
               placeholder="What's your email?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
@@ -59,6 +104,7 @@ const Contact = () => {
               rows="7"
               name="message"
               value={form.message}
+              onChange={handleChange}
               placeholder="What do you want to say?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
@@ -71,9 +117,11 @@ const Contact = () => {
           </button>
         </form>
       </motion.div>
-      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
         <EarthCanvas />
-
       </motion.div>
     </div>
   );
